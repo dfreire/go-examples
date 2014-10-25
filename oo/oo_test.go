648ab5b02aad5fe7640472852572e76804ecdeee
dfreire <dario.freire@gmail.com>
2014-10-25 20:37:54 +0100

    update

---
+++ oo/oo_test.go
@@ -0,0 +1,34 @@
+package oo_test
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+type Foo struct{}
+
+func (self Foo) Ping() string {
+	return "Pong"
+}
+
+func (self Foo) Hello() string {
+	return "World"
+}
+
+type Bar struct {
+	Foo
+}
+
+func (self Bar) Hello() string {
+	return "World!"
+}
+
+func Test(t *testing.T) {
+	foo := Foo{}
+	assert.Equal(t, "Pong", foo.Ping())
+	assert.Equal(t, "World", foo.Hello())
+
+	bar := Bar{}
+	assert.Equal(t, "Pong", bar.Ping())
+	assert.Equal(t, "World!", bar.Hello())
+}
