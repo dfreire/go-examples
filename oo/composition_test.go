869faebe772e529fd0749a63f44a0a7d0d82bf8c
dfreire <dario.freire@gmail.com>
2014-10-27 15:37:30 +0000

    update

--- oo/composition_test.go
+++ oo/composition_test.go
@@ -5,30 +5,60 @@ import (
 	"testing"
 )
 
-type Foo struct{}
-
-func (self Foo) Ping() string {
-	return "Pong"
+type Shape interface {
+	numOfCorners() int
+	equilateral() bool
 }
 
-func (self Foo) Hello() string {
-	return "World"
+type HasFourCorners struct{}
+
+func (self HasFourCorners) numOfCorners() int {
+	return 4
 }
 
-type Bar struct {
-	Foo
+type HasThreeCorners struct{}
+
+func (self HasThreeCorners) numOfCorners() int {
+	return 3
 }
 
-func (self Bar) Hello() string {
-	return "World!"
+type IsEquilateral struct{}
+
+func (self IsEquilateral) equilateral() bool {
+	return true
+}
+
+type IsntEquilateral struct{}
+
+func (self IsntEquilateral) equilateral() bool {
+	return false
+}
+
+type Square struct {
+	HasFourCorners
+	IsEquilateral
+}
+
+type Rectangle struct {
+	HasFourCorners
+	IsntEquilateral
+}
+
+type RightTriangle struct {
+	HasThreeCorners
+	IsntEquilateral
 }
 
 func TestComposition(t *testing.T) {
-	foo := Foo{}
-	assert.Equal(t, "Pong", foo.Ping())
-	assert.Equal(t, "World", foo.Hello())
+	var square Shape = Square{}
+	assert.Equal(t, 4, square.numOfCorners())
+	assert.Equal(t, true, square.equilateral())
 
-	bar := Bar{}
-	assert.Equal(t, "Pong", bar.Ping())
-	assert.Equal(t, "World!", bar.Hello())
+	var rectangle Shape = Rectangle{}
+	assert.Equal(t, 4, rectangle.numOfCorners())
+	assert.Equal(t, false, rectangle.equilateral())
+
+	var triangle Shape = RightTriangle{}
+	assert.Equal(t, 3, triangle.numOfCorners())
+	assert.Equal(t, false, triangle.equilateral())
 }
