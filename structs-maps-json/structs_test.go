ffd2bea035600ea79f6375eac3b3391268c9dbf0
dfreire <dario.freire@gmail.com>
2014-10-28 21:40:02 +0000

    update

--- structs-maps-json/structs_test.go
+++ structs-maps-json/structs_test.go
@@ -18,6 +18,36 @@ type Author struct {
 	Nationality string `json:"nationality"`
 }
 
+const testJson string = `[{
+	"title": "O ano da morte de Ricardo Reis",
+	"year": 1984,
+	"author": {
+		"name": "José Saramago",
+		"nationality": "Portugal"
+	}
+}, {
+	"title": "Ensaio sobre a cegueira",
+	"year": 1995,
+	"author": {
+		"name": "José Saramago",
+		"nationality": "Portugal"
+	}
+}, {
+	"title": "El mal de Montano",
+	"year": 2002,
+	"author": {
+		"name": "Enrique Vila-Matas",
+		"nationality": "Spain"
+	}
+}, {
+	"title": "Paris no se acaba nunca",
+	"year": 2003,
+	"author": {
+		"name": "Enrique Vila-Matas",
+		"nationality": "Spain"
+	}
+}]`
+
 func Test(t *testing.T) {
 	assert.Nil(t, nil)
 
