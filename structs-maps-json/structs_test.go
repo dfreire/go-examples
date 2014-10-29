dc28251367db73c6e81089b5459230f08cbfc62a
dfreire <dario.freire@gmail.com>
2014-10-29 12:38:19 +0000

    update

--- structs-maps-json/structs_test.go
+++ structs-maps-json/structs_test.go
@@ -2,7 +2,10 @@ package structs_test
 
 import (
 	"encoding/json"
+	// "fmt"
 	"github.com/fatih/structs"
+	// "reflect"
+	// "github.com/markbates/going/nulls"
 	"github.com/mitchellh/mapstructure"
 	"github.com/stretchr/testify/assert"
 	"os"
@@ -14,13 +17,12 @@ type Author struct {
 	Name        string                   `json:"name"`
 	Nationality string                   `json:"nationality"`
 	Books       []map[string]interface{} `json:"books"`
+	//Books       []Book `json:"books"`
 }
 
-//Books       []Book `json:"books"`
-
 type Book struct {
-	Title string `json:"title"`
-	Year  int    `json:"year"`
+	Title string `json:"title" structs:"title"`
+	Year  int    `json structs:"year"`
 }
 
 const jsonData string = `{
@@ -31,33 +33,30 @@ const jsonData string = `{
 		"year": 2002
 	}, {
 		"title": "Paris no se acaba nunca"
+	}, {
+		"title": "Doctor Pasavento",
+		"year": 2005,
+		"publisher": "Anagrama"
 	}]
 }`
 
 func Test(t *testing.T) {
 	assert.Nil(t, nil)
 
-	/*
-		catalog := []Author{
-			Author{"José Saramago", "Portugal", []Book{
-				Book{"O ano da morte de Ricardo Reis", 1984},
-				Book{"Ensaio sobre a cegueira", 1995},
-			}},
-			Author{"Enrique Vila-Matas", "Spain", []Book{
-				Book{"El mal de Montano", 2002},
-				Book{"Paris no se acaba nunca", 2003},
-			}},
-		}
-	*/
-
 	author := Author{}
 	assert.Nil(t, json.NewDecoder(strings.NewReader(jsonData)).Decode(&author))
-	//json.NewEncoder(os.Stdout).Encode(author)
+	json.NewEncoder(os.Stdout).Encode(author)
 
-	books := []Book{}
-	assert.Nil(t, mapstructure.Decode(author.Books, &books))
-	//json.NewEncoder(os.Stdout).Encode(books)
-
-	json.NewEncoder(os.Stdout).Encode(books)
-	json.NewEncoder(os.Stdout).Encode(structs.Map(author))
+	for _, bookMap := range author.Books {
+		book := Book{}
+		assert.Nil(t, mapstructure.Decode(bookMap, &book))
+		if bookMap["year"] == nil {
+			assert.Equal(t, 0, book.Year)
+		} else {
+			assert.Equal(t, bookMap["year"], book.Year)
+		}
+		// json.NewEncoder(os.Stdout).Encode(bookMap)
+		json.NewEncoder(os.Stdout).Encode(structs.Map(book))
+		//fmt.Println(reflect.TypeOf(&book).Elem().Field(0).Tag.Get("json"))
+	}
 }
