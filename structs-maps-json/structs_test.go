ac67a2c7799ea3f8e49a04e4cb744d14aa617bc4
dfreire <dario.freire@gmail.com>
2014-10-29 11:30:08 +0000

    update

--- structs-maps-json/structs_test.go
+++ structs-maps-json/structs_test.go
@@ -2,63 +2,62 @@ package structs_test
 
 import (
 	"encoding/json"
+	"github.com/fatih/structs"
+	"github.com/mitchellh/mapstructure"
 	"github.com/stretchr/testify/assert"
 	"os"
+	"strings"
 	"testing"
 )
 
-type Book struct {
-	Title  string `json:"title"`
-	Year   int    `json:"year"`
-	Author `json:"author"`
-}
-
 type Author struct {
-	Name        string `json:"name"`
-	Nationality string `json:"nationality"`
+	Name        string                   `json:"name"`
+	Nationality string                   `json:"nationality"`
+	Books       []map[string]interface{} `json:"books"`
 }
 
-const testJson string = `[{
-	"title": "O ano da morte de Ricardo Reis",
-	"year": 1984,
-	"author": {
-		"name": "José Saramago",
-		"nationality": "Portugal"
-	}
-}, {
-	"title": "Ensaio sobre a cegueira",
-	"year": 1995,
-	"author": {
-		"name": "José Saramago",
-		"nationality": "Portugal"
-	}
-}, {
-	"title": "El mal de Montano",
-	"year": 2002,
-	"author": {
-		"name": "Enrique Vila-Matas",
-		"nationality": "Spain"
-	}
-}, {
-	"title": "Paris no se acaba nunca",
-	"year": 2003,
-	"author": {
-		"name": "Enrique Vila-Matas",
-		"nationality": "Spain"
-	}
-}]`
+//Books       []Book `json:"books"`
+
+type Book struct {
+	Title string `json:"title"`
+	Year  int    `json:"year"`
+}
+
+const jsonData string = `{
+	"name": "Enrique Vila-Matas",
+	"nationality": "Spain",
+	"books": [{
+		"title": "El mal de Montano",
+		"year": 2002
+	}, {
+		"title": "Paris no se acaba nunca"
+	}]
+}`
 
 func Test(t *testing.T) {
 	assert.Nil(t, nil)
 
-	jose := Author{"José Saramago", "Portugal"}
-	enrique := Author{"Enrique Vila-Matas", "Spain"}
+	/*
+		catalog := []Author{
+			Author{"José Saramago", "Portugal", []Book{
+				Book{"O ano da morte de Ricardo Reis", 1984},
+				Book{"Ensaio sobre a cegueira", 1995},
+			}},
+			Author{"Enrique Vila-Matas", "Spain", []Book{
+				Book{"El mal de Montano", 2002},
+				Book{"Paris no se acaba nunca", 2003},
+			}},
+		}
+	*/
 
-	books := []Book{
-		Book{"O ano da morte de Ricardo Reis", 1984, jose},
-		Book{"Ensaio sobre a cegueira", 1995, jose},
-		Book{"El mal de Montano", 2002, enrique},
-		Book{"Paris no se acaba nunca", 2003, enrique},
-	}
+	author := Author{}
+	assert.Nil(t, json.NewDecoder(strings.NewReader(jsonData)).Decode(&author))
+	//json.NewEncoder(os.Stdout).Encode(author)
+
+	books := []Book{}
+	assert.Nil(t, mapstructure.Decode(author.Books, &books))
+	//json.NewEncoder(os.Stdout).Encode(books)
+
 	json.NewEncoder(os.Stdout).Encode(books)
+	json.NewEncoder(os.Stdout).Encode(structs.Map(author))
 }
