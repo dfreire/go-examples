fd85031ee8d0e09f30c33f617cc7d24e86e10e2d
dfreire <dario.freire@gmail.com>
2014-11-04 12:38:07 +0000

    update

--- pointers/pointers_test.go
+++ pointers/pointers_test.go
@@ -7,27 +7,50 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+type Everything struct {
+	Library
+	Catalog
+}
+
+type Library []*Book
+
+type Catalog []*Book
+
 type Book struct {
-	Title     string
-	AuthorPtr *Author
+	Title  string
+	Author *Author
 }
 
 type Author struct {
 	Name string
 }
 
-func Test(t *testing.T) {
-	author1 := &Author{"Enrique Vila-Matas"}
-	book1 := &Book{"El mal de Montano", author1}
-	assert.Equal(t, book1.AuthorPtr, author1)
+func TestSerializtion(t *testing.T) {
+	a1 := &Author{"Enrique Vila-Matas"}
+	b1 := &Book{"El mal de Montano", a1}
+	b2 := &Book{"Paris no se acaba nunca", a1}
+	l1 := Library{b1, b2}
+	c1 := Catalog{b1}
+	before := &Everything{l1, c1}
 
-	encoded, _ := json.Marshal(book1)
+	encoded, _ := json.Marshal(before)
 
-	book2 := &Book{}
-	json.Unmarshal(encoded, book2)
+	after := &Everything{}
+	json.Unmarshal(encoded, after)
 
-	assert.Equal(t, author1, book2.AuthorPtr)
-	assert.Equal(t, book1.AuthorPtr, book2.AuthorPtr)
-	assert.Equal(t, book1.Title, book2.Title)
-	assert.Equal(t, book1, book2)
+	assert.Equal(t, after, before)
+	assert.Equal(t, after.Library[0].Title, before.Library[0].Title)
+
+	// note that attention, although the before author is equal to the after author, in reality there are now two different structs in memory
+	var beforeAuthorPtr *Author = before.Library[0].Author
+	var afterAuthorPtr *Author = after.Library[0].Author
+
+	var beforeAuthor Author = *beforeAuthorPtr
+	var afterAuthor Author = *afterAuthorPtr
+
+	assert.Equal(t, afterAuthor, beforeAuthor)
+	assert.Equal(t, afterAuthor.Name, beforeAuthor.Name)
+
+	afterAuthor.Name = "E. Vila-Matas"
+	assert.NotEqual(t, afterAuthor.Name, beforeAuthor.Name)
 }
